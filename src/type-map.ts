b60e91f5b36d9c3c13aa883ca045a77c2dcaab70
bastien <method1990@gmail.com>
2022-02-10 01:02:58 +0100

    more on type

--- src/type-map.ts
+++ src/type-map.ts
@@ -14,3 +14,41 @@ type PageInfo = {
 type User = {
     [k in keyof GlobalState['user']]: GlobalState['user'][k]
 }
+
+type keys = keyof GlobalState;
+
+const n: keys = "page";
+
+type CustomPick<T, K extends keyof T> = { [k in K]: T[k] }
+
+type Url = CustomPick<GlobalState, "url">
+
+type Foo = {
+    type: 'foo'
+}
+
+type Bar = {
+    type: 'bar'
+}
+
+type Baz = Foo | Bar
+
+const baz: Baz = {
+    type: "foo"
+}
+
+type FooBazType = Baz['type']
+
+
+function getSomething(): {phone: string, name: string} {
+    return {
+        phone: '012345566',
+        name: 'foo'
+    }
+}
+
+// retourne uniquement le type de retour
+type SomethingResponse = ReturnType<typeof getSomething>
+
+// Retourne toute la signature
+type tt = typeof getSomething
